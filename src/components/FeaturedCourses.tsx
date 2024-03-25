feb4742c00f07cdae56de095d1de6eae2c25102e
Kriti <kritigupta2024@gmail.com>
2024-03-26 02:59:34 +0530

    modified courses

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link"
-import courseData from "../data/music_courses.json"
+import courseData from "../data/tech_courses.json"
 import { BackgroundGradient } from "./ui/background-gradient"
 
 interface Course{
